3cb0e1964b562aacdcfd9bf11fd6071153beb2f3
JacobStrabo <60807004+JacobStrabo@users.noreply.github.com>
2020-04-04 16:56:00 -0700

    Create strabo_test.py

---
+++ dataproject/strabo_test.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Data
+n_groups = 4
+Home_Win = (0.2, 0.3, 0.4, -0.2591842105263158)
+Draw = (0.1, 0.2, 0.3, -0.01960526315789468)
+Away_Win = (0, 0.02, 0.1, 0.050473684210526226)
+
+# Plot
+fig, ax = plt.subplots()
+index = np.arange(n_groups)
+bar_width = 0.1
+opacity = 0.7
+
+rects1 = plt.bar(index, Home_Win, bar_width,
+alpha=opacity,
+color='violet',
+label='Home Win')
+
+rects2 = plt.bar(index + bar_width, Draw, bar_width,
+alpha=opacity,
+color='hotpink',
+label='Draw')
+
+rects3 = plt.bar(index + bar_width + bar_width, Away_Win, bar_width,
+alpha=opacity,
+color='magenta',
+label='Away Win')
+
+plt.xlabel('Season')
+plt.ylabel('Return in %')
+plt.title('Premier League')
+plt.xticks(index + bar_width, ('15/16', '16/17', '17/18', '18/19'))
+plt.legend()
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = 'Helvetica'
+
+plt.tight_layout()
+plt.show()
