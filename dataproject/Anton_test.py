85c2994410c338dd7fd752f6f8acd34f7075abef
notnasobe666 <soeberg.anton@gmail.com>
2020-04-05 11:01:49 +0200

    anton

--- dataproject/Anton_test.py
+++ dataproject/Anton_test.py
@@ -15,7 +15,7 @@ usecols=['Date','HomeTeam','AwayTeam','FTHG','FTAG','FTR','B365H','B365D','B365A
 raw_input.head(20)
 
 # b. convert date from string to datetimes
-raw_input['Date'] = pd.to_datetime(raw_input['Date'])
+raw_input['Date'] = pd.to_datetime(raw_input['Date'], dayfirst = True)
 print(raw_input)
 
 # c. Summary statistics
@@ -40,25 +40,25 @@ print(away)
 df_w_seasons = pd.DataFrame(raw_input)
 season = []
 for row in df_w_seasons['Date']: 
-    if row > datetime(2009, 8, 15):
+    if row < datetime(2009, 7, 15):
         season.append('09/10')
-    elif row > datetime(2010, 8, 13):
+    elif row < datetime(2010, 7, 13):
         season.append('10/11')
-    elif row > datetime(2011, 8, 12):
+    elif row < datetime(2011, 7, 12):
         season.append('11/12')
-    elif row > datetime(2012, 8, 17):
+    elif row < datetime(2012, 7, 17):
         season.append('12/13')
-    elif row > datetime(2013, 8, 16):
+    elif row < datetime(2013, 7, 16):
         season.append('13/14')
-    elif row > datetime(2014, 8, 15):
+    elif row < datetime(2014, 7, 15):
         season.append('14/15')
-    elif row > datetime(2015, 8, 15):
+    elif row < datetime(2015, 7, 15):
         season.append('15/16')
-    elif row > datetime(2016, 8, 12):
+    elif row < datetime(2016, 7, 12):
         season.append('16/17')
-    elif row > datetime(2017, 8, 10):
+    elif row < datetime(2017, 7, 10):
         season.append('17/18')
-    elif row > datetime(2018, 8, 9):
+    elif row < datetime(2018, 7, 9):
         season.append('18/19')
     else:
         season.append('lort')
