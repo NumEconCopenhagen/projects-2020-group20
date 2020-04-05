d3af429228b507fb5de6f2d036efa6f0426a9a15
JacobStrabo <60807004+JacobStrabo@users.noreply.github.com>
2020-04-04 19:10:31 -0700

    Update strabos_test.py

--- dataproject/strabos_test.py
+++ dataproject/strabos_test.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Data
-n_groups = 4
-Home_Win = (0.2, 0.3, 0.4, -0.2591842105263158)
-Draw = (0.1, 0.2, 0.3, -0.01960526315789468)
-Away_Win = (0, 0.02, 0.1, 0.050473684210526226)
+n_groups = 10
+Home_Win = (0, 0, 0, 0, 0, 0, 0.2, 0.3, 0.4, -0.2591842105263158)
+Draw = (0, 0, 0, 0, 0, 0, 0.1, 0.2, 0.3, -0.01960526315789468)
+Away_Win = (0, 0, 0, 0, 0, 0, 0, 0.02, 0.1, 0.050473684210526226)
 
 # Plot
 fig, ax = plt.subplots()
 index = np.arange(n_groups)
-bar_width = 0.1
-opacity = 0.7
+bar_width = 0.2
+opacity = 1
 
 rects1 = plt.bar(index, Home_Win, bar_width,
 alpha=opacity,
@@ -29,9 +29,9 @@ color='magenta',
 label='Away Win')
 
 plt.xlabel('Season')
-plt.ylabel('Return in %')
+plt.ylabel('Profit in %')
 plt.title('Premier League')
-plt.xticks(index + bar_width, ('15/16', '16/17', '17/18', '18/19'))
+plt.xticks(index + bar_width, ('09/10', '10/11', '11/12', '12/13', '13/14', '14/15', '15/16', '16/17', '17/18', '18/19'))
 plt.legend()
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = 'Helvetica'
