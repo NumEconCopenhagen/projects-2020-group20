d041ad629e5f16b17f69fbedd8823a0859f3d246
notnasobe666 <soeberg.anton@gmail.com>
2020-04-05 10:48:26 +0200

    anton

--- dataproject/Anton_test.py
+++ dataproject/Anton_test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 
 # autoreload modules when code is run
@@ -13,47 +14,51 @@ raw_input = pd.read_csv('PL_all_seasons.csv', \
 usecols=['Date','HomeTeam','AwayTeam','FTHG','FTAG','FTR','B365H','B365D','B365A'])
 raw_input.head(20)
 
-# b. Summary statistics
+# b. convert date from string to datetimes
+raw_input['Date'] = pd.to_datetime(raw_input['Date'])
+print(raw_input)
+
+# c. Summary statistics
 raw_input.describe()
 
 # draw dataframe
 X_draw = pd.DataFrame(raw_input)
-draws = X_draw.loc[X_draw['FTR']=='D',['B365H','B365D','B365A']]
+draws = X_draw.loc[X_draw['FTR']=='D',['B365D']]
 print(draws)
 
 # home dataframe
 X_home = pd.DataFrame(raw_input)
-home = X_home.loc[X_home['FTR']=='H',['B365H','B365D','B365A']]
+home = X_home.loc[X_home['FTR']=='H',['B365H']]
 print(home)
 
 # away dataframe
 X_away = pd.DataFrame(raw_input)
-away = X_away.loc[X_away['FTR']=='A',['B365H','B365D','B365A']]
+away = X_away.loc[X_away['FTR']=='A',['B365A']]
 print(away)
 
 # append seasons
 df_w_seasons = pd.DataFrame(raw_input)
 season = []
 for row in df_w_seasons['Date']: 
-    if row > '14/08/09':
+    if row > datetime(2009, 8, 15):
         season.append('09/10')
-    elif row > '13/08/10':
+    elif row > datetime(2010, 8, 13):
         season.append('10/11')
-    elif row > '12/08/11':
+    elif row > datetime(2011, 8, 12):
         season.append('11/12')
-    elif row > '17/08/12':
+    elif row > datetime(2012, 8, 17):
         season.append('12/13')
-    elif row > '16/08/13':
+    elif row > datetime(2013, 8, 16):
         season.append('13/14')
-    elif row > '15/08/14':
+    elif row > datetime(2014, 8, 15):
         season.append('14/15')
-    elif row > '07/08/15':
+    elif row > datetime(2015, 8, 15):
         season.append('15/16')
-    elif row > '12/08/16':
+    elif row > datetime(2016, 8, 12):
         season.append('16/17')
-    elif row > '10/08/17':
+    elif row > datetime(2017, 8, 10):
         season.append('17/18')
-    elif row > '09/08/18':
+    elif row > datetime(2018, 8, 9)':
         season.append('18/19')
     else:
         season.append('lort')
